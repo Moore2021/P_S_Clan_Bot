6ca71292bc2fdfa01b6db29203f44f193e8ad0ae
Pan <thefryingpan2021@gmail.com>
2023-11-19 12:59:27 -0500

    Update app.js

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ app.post('/interactions', async function (req, res) {
     if (name === 'lfg') {
       const { options } = data
       const message = options[0].value
-      const channel = options[1].value ? options[1].value : false
+      const channel = options[1] ? options[1].value : false
       const embed = {
         "type": "rich",
         "title": `${member.nick ? member.nick : member.user.global_name} - LFG`,
